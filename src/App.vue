44a067ac5c0c26528c4a1fa2b78ff8f576d6cdfb
CIKADA <adel.garipovazn@mail.ru>
2023-07-21 01:47:37 +0300

    Change App component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <script setup lang="ts">
+import TheHeader from '@/components/header/TheHaeder.vue';
+import AppButton from '@/components/button/AppButton.vue';
+import CafesList from '@/components/cafes/CafesList.vue';
+
+  const clickOnButton = () => {
+    console.log('clickOnButton');
+  }
 
 </script>
 
 <template>
-  <h1 class="bg-red-400 text-3xl underline">
-    Hello from Tailwind
-  </h1>
-  <h1 class="bg-red-400 text-3xl font-medium underline">
-    Hello from Tailwind
-  </h1>
-    <h1 class="bg-red-400 text-3xl font-bold underline">
-    Hello from Tailwind
-  </h1>
+  <TheHeader />
+  <div class="container mx-auto p-4">
+    <AppButton
+      class="border-2 border-red-500 text-red-500 hover:bg-red-500 hover:text-white text-2xl mx-auto my-10 px-8 py-4 transition duration-300 ease-in-out hover:shadow-lg"
+      @click-on-button="clickOnButton"
+    >Хочу есть!
+    </AppButton>
+    <CafesList />
+  </div>
 </template>

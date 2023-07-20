88baa8ec590aed5b5e3f97eef5a3449ea6089abd
CIKADA <adel.garipovazn@mail.ru>
2023-07-21 02:11:58 +0300

    Create Badge component

---
+++ src/components/badge/AppBadge.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const emit = defineEmits(['click-on-badge']);
+
+const clickOnBadge = () => {
+  emit('click-on-badge');
+};
+</script>
+
+<template>
+  <div class="px-3 py-3 rounded-full  cursor-pointer transition duration-300 ease-in-out" @click="clickOnBadge">
+    <slot />
+  </div>
+</template>

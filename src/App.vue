25bc15b0bcdc5cba6289a8d48bda2e0ca31b05f5
CIKADA <adel.garipovazn@mail.ru>
2023-07-20 15:37:13 +0300

    Init project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1 class="bg-red-400 text-3xl underline">
+    Hello from Tailwind
+  </h1>
+  <h1 class="bg-red-400 text-3xl font-medium underline">
+    Hello from Tailwind
+  </h1>
+    <h1 class="bg-red-400 text-3xl font-bold underline">
+    Hello from Tailwind
+  </h1>
+</template>

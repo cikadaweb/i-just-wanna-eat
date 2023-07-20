6ca1885a5a567b2803906ba86ac4e39283661466
CIKADA <adel.garipovazn@mail.ru>
2023-07-21 01:47:51 +0300

    Create button component

---
+++ src/components/button/AppButton.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+  const emit = defineEmits(['click-on-button']);
+
+  defineProps({
+    type: {
+      type: String as () => 'button' | 'submit' | 'reset',
+      default: 'button',
+    },
+    disabled: {
+      type: Boolean,
+      required: false,
+    },
+  });
+
+  const clickOnButton = () => {
+    emit('click-on-button');
+  };
+</script>
+
+<template>
+  <button 
+    class="btn"
+    :type="type"
+    :disabled="disabled"
+    @click="clickOnButton"
+  >
+    <slot />
+  </button>
+</template>

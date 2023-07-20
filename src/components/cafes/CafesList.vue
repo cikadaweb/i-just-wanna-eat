e75f69bea7694a840759e8b0f22010ea9e42a42b
CIKADA <adel.garipovazn@mail.ru>
2023-07-21 02:11:05 +0300

    Create CafesList

---
+++ src/components/cafes/CafesList.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import CafeItem from '@/components/cafes/CafeItem.vue';
+
+import { useCafesStore } from '@/store/cafes';
+
+  const cafesStore = useCafesStore();
+  cafesStore.fetchAllCafes();
+
+</script>
+
+<template>
+  <div class="cafe-list">
+    <div class="cafe-list__row flex flex-wrap -mx-4">
+      <div
+        class="cafe-list__column w-full sm:w-1/2 xl:w-1/3 p-4"
+        v-for="cafe in cafesStore.getCafesList" :key="cafe.id"
+      >
+        <CafeItem :item="cafe" />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/header/TheHaeder.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header class="bg-red-500 text-slate-50 shadow-sm">
+    <div class="container flex justify-between mx-auto p-4">
+      <NuxtLink class="text-xl font-bold" to="/">I just wanna eat</NuxtLink>
+      <!-- <nav>
+        <ul class="flex items-center gap-4">
+          <li><NuxtLink to="/">Главная</NuxtLink></li>
+          <li><NuxtLink to="https://github.com/cikadaweb">Обо мне</NuxtLink></li>
+        </ul>
+      </nav> -->
+    </div>
+  </header>
+</template>
+
+<style scoped>
+</style>

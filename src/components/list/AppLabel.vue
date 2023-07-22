8de963a6293497363a06bea0ebf15e1bf04e84be
CIKADA <adel.garipovazn@mail.ru>
2023-07-22 15:23:07 +0300

    Create list label

---
+++ src/components/list/AppLabel.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="inline-block">
+    <div class="w-[25px] h-[25px] rounded-full bg-black flex items-center justify-center">
+      <div class="w-[7px] h-[7px] rounded-full bg-white"></div>
+    </div>
+  </div>
+</template>

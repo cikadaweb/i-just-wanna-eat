bc275176bdb8548af1c569b23d9f2599bd937587
CIKADA <adel.garipovazn@mail.ru>
2023-07-22 15:23:43 +0300

    Add AppModal handlers

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,54 @@
 <script setup lang="ts">
 import TheHeader from '@/components/header/TheHaeder.vue';
 import AppButton from '@/components/button/AppButton.vue';
+import AppDivider from '@/components/divider/AppDivider.vue';
 import CafesList from '@/components/cafes/CafesList.vue';
+import AppModal from '@/components/modal/AppModal.vue';
+
+import scrollController from '@/utils/modal-scroll';
+
+import { ref, watch, onBeforeUnmount } from 'vue';
+import { useCafesStore } from '@/store/cafes';
+
+
+  const cafesStore = useCafesStore();
 
   const clickOnButton = () => {
-    console.log('clickOnButton');
+    cafesStore.fetchRandomCafe();
+    isShowModal.value = true;
+  };
+
+  const modalRef = ref(null)
+  const isShowModal = ref(false);
+
+  const closeModal = () => {
+    isShowModal.value = false;
+  };
+  
+  const handleOutsideModalClick = (event: event) => {
+    const modal = modalRef.value.$el;
+    console.log(modal)
+    if (event.target === modal || event.code === 'Escape') {
+      closeModal();
+    }
   }
+
+  watch(isShowModal, (value: boolean) => {
+    if (value) {
+      scrollController.disabledScroll();
+      document.addEventListener('click', handleOutsideModalClick);
+      window.addEventListener('keydown', handleOutsideModalClick);
+    } else {
+      scrollController.enabledScroll();
+      document.removeEventListener('click', handleOutsideModalClick);
+      window.removeEventListener('keydown', handleOutsideModalClick);
+    }
+  });
+
+  onBeforeUnmount(() => {
+    document.removeEventListener('click', handleOutsideModalClick);
+    document.removeEventListener('keydown', handleOutsideModalClick);
+  });
 
 </script>
 
@@ -17,6 +60,16 @@ import CafesList from '@/components/cafes/CafesList.vue';
       @click-on-button="clickOnButton"
     >Хочу есть!
     </AppButton>
-    <CafesList />
+
+    <AppDivider />
+
+    <CafesList class="mt-5"/>
+
+    <AppModal
+      :isShowModal="isShowModal"
+      @close-modal="closeModal"
+      ref="modalRef"
+    />
+
   </div>
 </template>

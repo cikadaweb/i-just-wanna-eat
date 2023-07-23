de21d931cf47a26e2b5cdb136590052d78b0b074
CIKADA <adel.garipovazn@mail.ru>
2023-07-23 13:13:41 +0300

    Create adaptive Picture component

---
+++ src/components/picture/AppPicture.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+  const props = defineProps({
+    imageSrc: {
+      type: String,
+      required: true,
+    },
+    imageName: {
+      type: String,
+      default: 'cafe'
+    },
+    imageSize: {
+      type: String,
+    },
+    rounded: {
+      type: Boolean,
+      default: false
+    }
+  });
+
+  const imagePictureSize = computed(() => {
+    if (props.imageSize) {
+      return `pt-[${props.imageSize}]`;
+    }
+    return 'pt-[300px]';
+  });
+
+</script>
+
+<template>
+  <div class="relative" :class="imagePictureSize">
+    <img
+      class="absolute top-0 left-0 w-full h-full object-cover"
+      :class="rounded ? 'rounded' : ''"
+      :src="imageSrc" :alt="`${imageName}-cafe`"
+    >
+  </div>
+</template>

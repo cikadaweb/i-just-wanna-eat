3f6da39c9d1a37aed99af4636f3bfd22e42935b1
CIKADA <adel.garipovazn@mail.ru>
2023-07-23 13:13:00 +0300

    Add Header links

--- src/components/header/TheHeader.vue
+++ src/components/header/TheHeader.vue
@@ -1,20 +1,12 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
   <header class="bg-red-500 text-slate-50 shadow-sm">
     <div class="container flex justify-between mx-auto p-4">
-      <NuxtLink class="text-xl font-bold" to="/">I just wanna eat</NuxtLink>
-      <!-- <nav>
+      <router-link class="text-xl font-bold" to="/">I just wanna eat</router-link>
+      <nav>
         <ul class="flex items-center gap-4">
-          <li><NuxtLink to="/">Главная</NuxtLink></li>
-          <li><NuxtLink to="https://github.com/cikadaweb">Обо мне</NuxtLink></li>
+          <li><router-link to="https://github.com/cikadaweb">Обо мне</router-link></li>
         </ul>
-      </nav> -->
+      </nav>
     </div>
   </header>
 </template>
-
-<style scoped>
-</style>

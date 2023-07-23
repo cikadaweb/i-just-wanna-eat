7fdc59b19075064bddccaca8c5974f299b79773c
CIKADA <adel.garipovazn@mail.ru>
2023-07-23 13:14:56 +0300

    Create default layout

---
+++ src/layouts/Default.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import TheHeader from '@/components/header/TheHeader.vue';
+
+</script>
+
+<template>
+  <div class="default-layout">
+    <TheHeader />
+    <main class="container mx-auto p-4">
+      <router-view />
+    </main>
+  </div>
+</template>
